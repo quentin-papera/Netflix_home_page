4295fefc6f636e4f9c8f3280549ae4fcdb9df1b7
Quentin Papera <qpapera@gmail.com>
2020-09-17 09:56:32 +1200

    First initial my bad

---
+++ src/main.js
@@ -0,0 +1,39 @@
+document.getElementById('buttonOne').addEventListener('click',
+function() {
+	document.querySelector('.bg-modal').style.display = 'flex'
+})
+
+document.querySelector('.close').addEventListener('click', 
+function(){
+	document.querySelector('.bg-modal').style.display = 'none'
+})
+
+document.getElementById('buttonTwo').addEventListener('click',
+function() {
+	document.querySelector('.bg-modal').style.display = 'flex'
+})
+
+document.querySelector('.close').addEventListener('click', 
+function(){
+	document.querySelector('.bg-modal').style.display = 'none'
+})
+
+document.getElementById('buttonThree').addEventListener('click',
+function() {
+	document.querySelector('.bg-modal').style.display = 'flex'
+})
+
+document.querySelector('.close').addEventListener('click', 
+function(){
+	document.querySelector('.bg-modal').style.display = 'none'
+})
+
+document.getElementById('buttonFour').addEventListener('click',
+function() {
+	document.querySelector('.bg-modal').style.display = 'flex'
+})
+
+document.querySelector('.close').addEventListener('click', 
+function(){
+	document.querySelector('.bg-modal').style.display = 'none'
+})
