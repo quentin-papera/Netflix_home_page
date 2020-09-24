1209b1988ae64dba14364391293639b5c76c2aae
Quentin Papera <qpapera@gmail.com>
2020-09-24 21:26:36 +1200

    Changed styling and added BS modal

--- src/main.js
+++ src/main.js
@@ -1,39 +1,39 @@
-document.getElementById('buttonOne').addEventListener('click',
-function() {
-	document.querySelector('.bg-modal').style.display = 'flex'
-})
+// document.getElementById('buttonOne').addEventListener('click',
+// function() {
+// 	document.querySelector('.bg-modal').style.display = 'flex'
+// })
 
-document.querySelector('.close').addEventListener('click', 
-function(){
-	document.querySelector('.bg-modal').style.display = 'none'
-})
+// document.querySelector('.close').addEventListener('click', 
+// function(){
+// 	document.querySelector('.bg-modal').style.display = 'none'
+// })
 
-document.getElementById('buttonTwo').addEventListener('click',
-function() {
-	document.querySelector('.bg-modal').style.display = 'flex'
-})
+// document.getElementById('buttonTwo').addEventListener('click',
+// function() {
+// 	document.querySelector('.bg-modal').style.display = 'flex'
+// })
 
-document.querySelector('.close').addEventListener('click', 
-function(){
-	document.querySelector('.bg-modal').style.display = 'none'
-})
+// document.querySelector('.close').addEventListener('click', 
+// function(){
+// 	document.querySelector('.bg-modal').style.display = 'none'
+// })
 
-document.getElementById('buttonThree').addEventListener('click',
-function() {
-	document.querySelector('.bg-modal').style.display = 'flex'
-})
+// document.getElementById('buttonThree').addEventListener('click',
+// function() {
+// 	document.querySelector('.bg-modal').style.display = 'flex'
+// })
 
-document.querySelector('.close').addEventListener('click', 
-function(){
-	document.querySelector('.bg-modal').style.display = 'none'
-})
+// document.querySelector('.close').addEventListener('click', 
+// function(){
+// 	document.querySelector('.bg-modal').style.display = 'none'
+// })
 
-document.getElementById('buttonFour').addEventListener('click',
-function() {
-	document.querySelector('.bg-modal').style.display = 'flex'
-})
+// document.getElementById('buttonFour').addEventListener('click',
+// function() {
+// 	document.querySelector('.bg-modal').style.display = 'flex'
+// })
 
-document.querySelector('.close').addEventListener('click', 
-function(){
-	document.querySelector('.bg-modal').style.display = 'none'
-})
+// document.querySelector('.close').addEventListener('click', 
+// function(){
+// 	document.querySelector('.bg-modal').style.display = 'none'
+// })
